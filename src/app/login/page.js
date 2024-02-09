5f96b6fb0729637449a2960a7ec27f941c5037b8
Joel Crasta <loejstarc@gmail.com>
2024-02-10 01:24:18 +0530

    added css to login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -20,7 +20,6 @@ const page = () => {
         return;
       } else {
         console.log("Login successful");
-
       }
     } catch (error) {
       console.error("Login error:", error.message);
@@ -30,38 +29,48 @@ const page = () => {
   return (
     <>
       <div className="h-screen flex flex-col justify-center items-center">
-        <h1>Login</h1>
-        <div className="text-black">
-          <form onSubmit={handleLogin}>
-            <div className="flex flex-col">
-              <input
-                type="email"
-                value={email}
-                placeholder="Email"
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-              />
-            </div>
-            <div className="flex flex-col">
-              <input
-                type="password"
-                value={password}
-                placeholder="Password"
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-            </div>
-            <div className="flex justify-center border-2 mt-4 text-white">
-              <button type="submit">Login</button>
-            </div>
-            <div>
-              <p className="text-white">
-                Dont have an account? <a href="/signup">SignUp</a>
-              </p>
-            </div>
-          </form>
+        <div className="border-2 h-96 w-80 flex flex-col justify-center shadow-lg shadow-white ">
+          <h1 className="text-center text-2xl mt-4 font-semibold">Login</h1>
+          <div className="p-4">
+            <form onSubmit={handleLogin}>
+              <div className="flex flex-col">
+                <input
+                  type="email"
+                  value={email}
+                  placeholder="Email"
+                  onChange={(e) => {
+                    setEmail(e.target.value);
+                  }}
+                  className="border-2 px-4 py-2 mb-4 bg-white bg-opacity-10"
+                />
+              </div>
+              <div className="flex flex-col">
+                <input
+                  type="password"
+                  value={password}
+                  placeholder="Password"
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  className="border-2 px-4 py-2 mb-4 bg-white bg-opacity-10"
+                />
+              </div>
+              <hr />
+              <div className="border-2 mt-4 hover:bg-white hover:text-black transition-all duration-200">
+                <button type="submit" className="w-full h-10 font-semibold">
+                  Login
+                </button>
+              </div>
+              <div>
+                <p className="mt-4 text-center font-light">
+                  Dont have an account?{" "}
+                  <a href="/signup" className="hover:text-red-500">
+                    SignUp
+                  </a>
+                </p>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </>
